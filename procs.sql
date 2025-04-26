211dac1349b232804d40df0fe9d77ac8bfe17087
Calianna <126907977+ccollins1656@users.noreply.github.com>
2025-04-26 14:33:17 -0700

    Added create_forum

--- procs.sql
+++ procs.sql
@@ -6,6 +6,16 @@ IN in_join_time DATETIME)
 BEGIN
 	INSERT INTO user (user_id, display_name, email, password, join_time)
     VALUES (in_user_id, in_display_name, in_email, in_password, in_join_time);
-	END;
+END;
+%%%
+@delimiter ;
+
+DROP PROCEDURE IF EXISTS create_forum;
+@delimiter %%%
+CREATE PROCEDURE create_forum(in in_class_name(10))
+BEGIN
+	INSERT INTO forum(in_class_name)
+	VALUES (class_name);
+END;
 %%%
 @delimiter ;
