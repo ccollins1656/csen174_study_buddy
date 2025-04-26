050313aae0e03f9c78fc756310eea8f2ec258b01
Calianna <126907977+ccollins1656@users.noreply.github.com>
2025-04-26 14:44:30 -0700

    Added message tables

--- tables.sql
+++ tables.sql
@@ -33,3 +33,27 @@ create table joined_forum(
     foreign key(user_id) references user(user_id) on delete cascade,
     foreign key(class_name) references forum(class_name) on delete cascade
 );
+
+drop table if exists forum_message;
+create table forum_message(
+	user_id varchar(10) not null,
+	class_name varchar(10) not null,
+	timestamp datetime not null,
+	text varchar(256) not null,
+
+	primary key(user_id, class_name, timestamp),
+	foreign key(user_id) references user(user_id) on delete set null,
+	foreign key(class_name) references forum(class_name) on delete cascade
+);
+
+drop table if exists direct_message;
+create table direct_message(
+	sending_user_id varchar(10) not null,
+	receiving_user_id varchar(10) not null,
+	timestamp datetime not null,
+	text varchar(256) not null,
+
+	primary key(sending_user_id, receiving_user_id, timestamp),
+	foreign key(sending_user_id) references user(user_id) on delete cascade,
+	foreign key(receiving_user_id) references user(user_id) on delete cascade
+);
