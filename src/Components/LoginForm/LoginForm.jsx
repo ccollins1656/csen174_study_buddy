ff98abb97f63dae129f973ee4facf2a86ad6acb4
Henrik Evers <henrik408ski@gmail.com>
2025-05-13 14:28:45 -0700

    Update LoginForm.jsx

--- src/Components/LoginForm/LoginForm.jsx
+++ src/Components/LoginForm/LoginForm.jsx
@@ -3,6 +3,22 @@ import './LoginForm.css';
 import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import { FaUser, FaLock } from "react-icons/fa";
+import axios from 'axios';
+
+async function tryLogin(email, password) {
+    const response = await axios.post('http://localhost:5000/login', {
+        "email": email,
+        "password": password
+    }).catch(function (e) {
+        console.log(e);
+        return false;
+    });
+    if (response.status === 204) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 const LoginForm = () => {
     const navigate = useNavigate();
@@ -10,7 +26,7 @@ const LoginForm = () => {
     const [password, setPassword] = useState('');
     const [error, setError] = useState('');
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
 
         // Check if email ends with @scu.edu
@@ -19,22 +35,17 @@ const LoginForm = () => {
             return;
         }
 
-        const users = JSON.parse(localStorage.getItem('users') || '[]');
-        const matchingUser = users.find(
-        (user) => user.email === email && user.password === password
-        );
+        let loggedIn = await tryLogin(email, password);
 
-        if (!matchingUser) {
+        if (loggedIn) {
+            // Proceed with login logic here
+            setError('Login succesful!');
+            //alert('Login successful!');
+            navigate('/welcome', { state: { email } });
+        } else {
             setError('Invalid email or password.');
-            return;
         }
 
-
-        // Proceed with login logic here
-        setError('');
-        //alert('Login successful!');
-        localStorage.setItem('currentUser', JSON.stringify({ email }));
-        navigate('/welcome', { state: { email } });
     };
 
     return (
@@ -81,4 +92,4 @@ const LoginForm = () => {
     );
 };
 
-export default LoginForm; 
+export default LoginForm;
