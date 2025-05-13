8ab61cf6dbea7d8a6d5667fa28459819e0516535
A113RAM <araman3@scu.edu>
2025-05-12 20:13:17 -0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,31 +1,23 @@
-// src/App.js
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { CookiesProvider } from 'react-cookie';
-import LoginPage from './LoginPage';
-import WelcomePage from './WelcomePage';
-import AddCoursePage from './AddCoursePage';
-import MessagesPage from './MessagesPage';
-import SettingsPage from './SettingsPage';
-import Navbar from './Navbar';
+import LoginForm from './Components/LoginForm/LoginForm';
+import RegisterForm from './Components/LoginForm/RegisterForm';
+import WelcomePage from './Components/LoginForm/WelcomePage';
+//import WelcomePage from './WelcomePage';
+//import AddCoursePage from './AddCoursePage';
+//import MessagesPage from './MessagesPage';
 
 function App() {
   return (
-    <CookiesProvider>
     <Router>
-      <Navbar />
       <Routes>
-        <Route path="/" element={<LoginPage />} />
+        <Route path="/" element={<LoginForm />} />
+        <Route path="/register" element={<RegisterForm />} />
         <Route path="/welcome" element={<WelcomePage />} />
-        <Route path="/add-course" element={<AddCoursePage />} />
-        <Route path="/messages" element={<MessagesPage />} />
-        <Route path="/settings" element={<SettingsPage />} />
       </Routes>
     </Router>
-    </CookiesProvider>
   );
 }
 
 export default App;
-
 
 
