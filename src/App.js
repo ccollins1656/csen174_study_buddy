522e1ef2a5185d5114c72b078cb2c6b52cfa9cb3
i-kennar <ian.kennar@icloud.com>
2025-05-13 14:36:24 -0700

    updated ui

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import LoginForm from './components/LoginForm/LoginForm';
 import RegisterForm from './components/LoginForm/RegisterForm';
 import AuthPage from './components/LoginForm/AuthForm';
 import WelcomePage from './components/LoginForm/WelcomePage';
+import AddCoursePage from './AddCoursePage'
+
 //import WelcomePage from './WelcomePage';
 //import AddCoursePage from './AddCoursePage';
 //import MessagesPage from './MessagesPage';
@@ -10,11 +12,13 @@ import WelcomePage from './components/LoginForm/WelcomePage';
 function App() {
   return (
     <Router>
+      {/* Add your navbar component here */}
       <Routes>
         <Route path="/" element={<LoginForm />} />
         <Route path="/register" element={<RegisterForm />} />
         <Route path="/auth" element={<AuthPage />} />
         <Route path="/welcome" element={<WelcomePage />} />
+        <Route path="/add-course" element={<AddCoursePage />} />
       </Routes>
     </Router>
   );
