61e5669106cabaa33bb0292bca96fa15b9238222
Henrik Evers <henrik408ski@gmail.com>
2025-05-19 23:22:15 -0700

    Login only accessible if logged out

--- src/Components/LoginForm/LoginForm.jsx
+++ src/Components/LoginForm/LoginForm.jsx
@@ -3,6 +3,7 @@ import './LoginForm.css';
 import { Link, useNavigate } from 'react-router-dom';
 import { FaUser, FaLock } from "react-icons/fa";
 import axios from 'axios';
+import { useSessionUnauth } from './useSessionAuth.js';
 
 async function tryLogin(email, password, remember) {
     const response = await axios.post('http://localhost:5000/login', {
@@ -21,6 +22,8 @@ async function tryLogin(email, password, remember) {
 }
 
 const LoginForm = () => {
+    useSessionUnauth();
+
     const navigate = useNavigate();
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
--- src/components/LoginForm/useSessionAuth.js
+++ src/components/LoginForm/useSessionAuth.js
@@ -2,6 +2,7 @@ import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
+// Target page is only accessible if logged in
 export function useSessionAuth() {
   const navigate = useNavigate();
 
@@ -19,6 +20,32 @@ export function useSessionAuth() {
       if (!response || !(response.status === 204)) {
           // Empty response or non-OK code
           navigate('/login');
+      }
+    }
+
+    sessionAuth(token);
+  })
+}
+
+// Target page is only accessible if not logged in
+export function useSessionUnauth() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem('session');
+  
+    async function sessionAuth(token) {
+      const response = await axios.post('http://localhost:5000/session', {
+          "token": token
+      }).catch(function (e) {
+          // Can't reach the server
+          console.log(e);
+      });
+      if (!response || !(response.status === 204)) {
+          // Empty response or non-OK code
+      }
+      else {
+        navigate('/welcome');
       }
     }
 
