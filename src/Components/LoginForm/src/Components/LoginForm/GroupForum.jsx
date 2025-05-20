8c1ac5ddbd304559af883a4e2167bae17abb9cde
A113RAM <araman3@scu.edu>
2025-05-19 22:23:43 -0700

    Add files via upload

---
+++ src/Components/LoginForm/src/Components/LoginForm/GroupForum.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from './Layout';
+
+const GroupForum = () => {
+    return (
+        <Layout>
+            <h1>Group Forum!</h1>
+            <p>This is the group forum for Study Buddy. Select an option from the sidebar.</p>
+        </Layout>
+    );
+};
+
+export default GroupForum;
---
+++ src/Components/LoginForm/src/Components/LoginForm/WelcomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from './Layout';
+
+const WelcomePage = () => {
+    return (
+        <Layout>
+            <h1>Welcome!</h1>
+            <p>This is the home page for Study Buddy. Select an option from the sidebar.</p>
+        </Layout>
+    );
+};
+
+export default WelcomePage;
+
