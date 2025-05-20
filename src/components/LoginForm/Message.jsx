68c7b0008d12d6dc5784a930d65848a4e197de10
Henrik Evers <henrik408ski@gmail.com>
2025-05-20 14:42:08 -0700

    Resolving last night's merge conflict
    Sorry for the stuff that got commented out because it's not compiling yet

---
+++ src/components/LoginForm/Message.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from './Layout';
+
+const Message = () => {
+    return (
+        <Layout>
+            <h1>Message Tutor/Student!</h1>
+            <p>This is the message tutor/student page for Study Buddy. Select an option from the sidebar.</p>
+        </Layout>
+    );
+};
+
+export default Message;
